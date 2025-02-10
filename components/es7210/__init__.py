0e05163a0f870b4a12781a13afcef7c41540e7e8
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-11 00:49:58 +0100

    Update __init__.py

--- components/es7210/__init__.py
+++ components/es7210/__init__.py
@@ -1,18 +1,20 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, microphone, i2s_audio
-from esphome.const import CONF_ID, CONF_BITS_PER_SAMPLE, CONF_SAMPLE_RATE, CONF_I2S_AUDIO_ID
+from esphome.const import CONF_ID, CONF_BITS_PER_SAMPLE, CONF_SAMPLE_RATE
 
 CODEOWNERS = ["@your_github_handle"]
 DEPENDENCIES = ["i2c", "i2s_audio", "microphone"]
 AUTO_LOAD = ["microphone"]
+
+CONF_I2S_AUDIO = "i2s_audio_id"
 
 es7210_ns = cg.esphome_ns.namespace("es7210")
 ES7210Microphone = es7210_ns.class_("ES7210Microphone", microphone.Microphone, cg.Component, i2c.I2CDevice)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(ES7210Microphone),
-    cv.Required(CONF_I2S_AUDIO_ID): cv.use_id(i2s_audio.I2SAudioComponent),
+    cv.Required(CONF_I2S_AUDIO): cv.use_id(i2s_audio.I2SAudioComponent),
     cv.Optional(CONF_SAMPLE_RATE, default="16000Hz"): cv.int_range(min=8000, max=48000),
     cv.Optional(CONF_BITS_PER_SAMPLE, default="16bit"): cv.one_of(16, 24, 32, int=True),
     cv.Optional("i2c_id"): cv.use_id(i2c.I2CComponent),
@@ -25,7 +27,7 @@ async def to_code(config):
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
 
-    i2s = await cg.get_variable(config[CONF_I2S_AUDIO_ID])
+    i2s = await cg.get_variable(config[CONF_I2S_AUDIO])
     cg.add(var.set_i2s_audio(i2s))
 
     cg.add(var.set_sample_rate(config[CONF_SAMPLE_RATE]))
