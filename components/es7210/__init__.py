935d4240d0c7f46118df9edf387b2c3ca093a607
youkorr <96953496+youkorr@users.noreply.github.com>
2025-02-11 14:39:16 +0100

    Update __init__.py

--- components/es7210/__init__.py
+++ components/es7210/__init__.py
@@ -1,51 +1,67 @@
 import esphome.codegen as cg
+from esphome.components import i2c
 import esphome.config_validation as cv
-from esphome.components import i2c, i2s_audio, microphone
-from esphome.const import CONF_ID, CONF_BITS_PER_SAMPLE, CONF_SAMPLE_RATE
+from esphome.const import CONF_BITS_PER_SAMPLE, CONF_ID, CONF_MIC_GAIN, CONF_SAMPLE_RATE
 
-DEPENDENCIES = ['i2c', 'i2s_audio']
-AUTO_LOAD = ['microphone']
-CODEOWNERS = ['@youkorr']
-
-CONF_I2S_AUDIO = "i2s_audio_id"
+CODEOWNERS = ["@kahrendt"]
+DEPENDENCIES = ["i2c"]
 
 es7210_ns = cg.esphome_ns.namespace("es7210")
-ES7210Microphone = es7210_ns.class_("ES7210Microphone", microphone.Microphone, cg.Component, i2c.I2CDevice)
+ES7210 = es7210_ns.class_("ES7210", cg.Component, i2c.I2CDevice)
 
-def validate_gpio_pin(value):
-    if isinstance(value, str) and value.startswith("GPIO"):
-        return int(value[4:])
-    return cv.int_(value)
 
-# Définissez d'abord votre schéma de configuration de base
-BASE_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(ES7210Microphone),
-    cv.Required(CONF_I2S_AUDIO): cv.use_id(i2s_audio.I2SAudioComponent),
-    cv.Optional(CONF_SAMPLE_RATE, default="16000Hz"): cv.frequency,
-    cv.Optional(CONF_BITS_PER_SAMPLE, default="16bit"): cv.one_of(16, 24, 32, int=True),
-    cv.Optional("i2c_id"): cv.use_id(i2c.I2CBus),
-    cv.Optional("i2s_din_pin"): validate_gpio_pin,
-    cv.Optional("adc_type", default="external"): cv.string_strict,
-}).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(0x40))
+es7210_bits_per_sample = es7210_ns.enum("ES7210BitsPerSample")
+ES7210_BITS_PER_SAMPLE_ENUM = {
+    16: es7210_bits_per_sample.ES7210_BITS_PER_SAMPLE_16,
+    24: es7210_bits_per_sample.ES7210_BITS_PER_SAMPLE_24,
+    32: es7210_bits_per_sample.ES7210_BITS_PER_SAMPLE_32,
+}
 
-# Enregistrez le composant en tant que plateforme de microphone
-CONFIG_SCHEMA = microphone.MICROPHONE_PLATFORM_SCHEMA.extend(BASE_SCHEMA)
+
+es7210_mic_gain = es7210_ns.enum("ES7210MicGain")
+ES7210_MIC_GAIN_ENUM = {
+    "0DB": es7210_mic_gain.ES7210_MIC_GAIN_0DB,
+    "3DB": es7210_mic_gain.ES7210_MIC_GAIN_3DB,
+    "6DB": es7210_mic_gain.ES7210_MIC_GAIN_6DB,
+    "9DB": es7210_mic_gain.ES7210_MIC_GAIN_9DB,
+    "12DB": es7210_mic_gain.ES7210_MIC_GAIN_12DB,
+    "15DB": es7210_mic_gain.ES7210_MIC_GAIN_15DB,
+    "18DB": es7210_mic_gain.ES7210_MIC_GAIN_18DB,
+    "21DB": es7210_mic_gain.ES7210_MIC_GAIN_21DB,
+    "24DB": es7210_mic_gain.ES7210_MIC_GAIN_24DB,
+    "27DB": es7210_mic_gain.ES7210_MIC_GAIN_27DB,
+    "30DB": es7210_mic_gain.ES7210_MIC_GAIN_30DB,
+    "33DB": es7210_mic_gain.ES7210_MIC_GAIN_33DB,
+    "34.5DB": es7210_mic_gain.ES7210_MIC_GAIN_34_5DB,
+    "36DB": es7210_mic_gain.ES7210_MIC_GAIN_36DB,
+    "37.5DB": es7210_mic_gain.ES7210_MIC_GAIN_37_5DB,
+}
+
+_validate_bits = cv.float_with_unit("bits", "bit")
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(ES7210),
+            cv.Optional(CONF_BITS_PER_SAMPLE, default="16bit"): cv.All(
+                _validate_bits, cv.enum(ES7210_BITS_PER_SAMPLE_ENUM)
+            ),
+            cv.Optional(CONF_MIC_GAIN, default="24DB"): cv.enum(
+                ES7210_MIC_GAIN_ENUM, upper=True
+            ),
+            cv.Optional(CONF_SAMPLE_RATE, default=16000): cv.int_range(min=1),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(i2c.i2c_device_schema(0x40))
+)
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await microphone.register_microphone(var, config)
     await i2c.register_i2c_device(var, config)
 
-    if CONF_I2S_AUDIO in config:
-        i2s = await cg.get_variable(config[CONF_I2S_AUDIO])
-        cg.add(var.set_i2s_audio(i2s))
-
-    cg.add(var.set_sample_rate(config[CONF_SAMPLE_RATE]))
     cg.add(var.set_bits_per_sample(config[CONF_BITS_PER_SAMPLE]))
-
-    if "i2s_din_pin" in config:
-        cg.add(var.set_i2s_din_pin(config["i2s_din_pin"]))
-
-    if "adc_type" in config:
-        cg.add(var.set_adc_type(config["adc_type"]))
+    cg.add(var.set_mic_gain(config[CONF_MIC_GAIN]))
+    cg.add(var.set_sample_rate(config[CONF_SAMPLE_RATE]))
